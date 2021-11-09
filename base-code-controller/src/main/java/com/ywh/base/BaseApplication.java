0d7dea417546c2925b819d77eda9ced64d3fbcfa
ywhnaxierizi <910328yuewenhao>
2021-11-09 17:39:12 +0800

    mybatis配置

--- base-code-controller/src/main/java/com/ywh/base/BaseApplication.java
+++ base-code-controller/src/main/java/com/ywh/base/BaseApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication(scanBasePackages = "com.ywh.base",exclude = {DataSourceAutoConfiguration.class})
+@MapperScan("com.ywh.base.common.dao")
 public class BaseApplication {
     public static void main(String[] args) {
         SpringApplication.run(BaseApplication.class, args);
